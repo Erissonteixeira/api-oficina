eb04e4f1a58a28bc0e3fbf3fc00481483563a4ba
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-21 16:54:52 -0300

    primeiro commit

---
+++ src/test/java/io/github/com/Erissonteixeira/api_crudoficina/ApiCrudoficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.Erissonteixeira.api_crudoficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudoficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

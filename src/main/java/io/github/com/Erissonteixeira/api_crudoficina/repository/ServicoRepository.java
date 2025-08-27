cbead6b27f71bf550d250ac6b88c4dce971a70b4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-27 14:53:47 -0300

    feat: adicionar ServicoRepository para operações com entidade Servico.

---
+++ src/main/java/io/github/com/Erissonteixeira/api_crudoficina/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package io.github.com.Erissonteixeira.api_crudoficina.repository;
+
+import io.github.com.Erissonteixeira.api_crudoficina.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}

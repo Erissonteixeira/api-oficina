4ed8d1203d44c5386edb044842aec2ffb61036bd
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-27 18:49:06 -0300

    feat: criar ServicoController com endpoints CRUD para Servico

---
+++ src/main/java/io/github/com/Erissonteixeira/api_crudoficina/controller/ServicoController.java
@@ -0,0 +1,39 @@
+package io.github.com.Erissonteixeira.api_crudoficina.controller;
+
+import io.github.com.Erissonteixeira.api_crudoficina.dto.ServicoRequestDTO;
+import io.github.com.Erissonteixeira.api_crudoficina.dto.ServicoResponseDTO;
+import io.github.com.Erissonteixeira.api_crudoficina.service.ServicoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicos")
+public class ServicoController {
+
+    private final ServicoService service;
+
+    public ServicoController(ServicoService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<ServicoResponseDTO> criar(@Valid @RequestBody ServicoRequestDTO dto){
+        return ResponseEntity.ok(service.criar(dto));
+    }
+    @GetMapping
+    public ResponseEntity<List<ServicoResponseDTO>> listarTodos(){
+        return ResponseEntity.ok(service.listarTodos());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ServicoResponseDTO> atualizar(@PathVariable Long id, @RequestBody ServicoRequestDTO dto) {
+        return service.atualizar(id, dto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        return service.excluir(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+}

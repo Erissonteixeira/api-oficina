5fd4b6b4dbc15ba926ee3515b062772850a837ca
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-02 13:56:10 -0300

    test: criar estrutura inicial de ServicoServiceTest

---
+++ src/test/java/io/github/com/Erissonteixeira/api_crudoficina/service/ServicoServiceTest.java
@@ -0,0 +1,14 @@
+package io.github.com.Erissonteixeira.api_crudoficina.service;
+
+import io.github.com.Erissonteixeira.api_crudoficina.repository.ServicoRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ServicoServiceTest {
+    @InjectMocks
+    private ServicoRepository repository;
+
+
+}
